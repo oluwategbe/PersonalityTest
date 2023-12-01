96d16fb2a66d302664840d0218a73e54bfcedb0e
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2023-12-01 05:30:22 +0100

    changes

--- src/utils/data.js
+++ src/utils/data.js
@@ -361,7 +361,7 @@ export const personalityData = [
       },
       {
         title: "Dissatisfaction",
-        desc: " Their constant search for improvement can lead to perpetual dissatisfaction.",
+        desc: "Their constant search for improvement can lead to perpetual dissatisfaction.",
       },
     ],
     career: {
@@ -404,81 +404,177 @@ export const personalityData = [
     male: "/images/entj-commander-s2-male.png",
     female: "/images/entj-commander-s2-female.png",
     short_desc:
-      "Known for their brilliant ideas and logic, they are problem-solving innovators.",
+      "Natural, strong-willed leaders who take charge of situations and achieve goals.",
     long_desc:
-      "INTPs are innovative thinkers with an insatiable thirst for knowledge. Driven by logical analysis and fascinated by underlying principles, they thrive in environments that value creative genius. Their keen eye for patterns and discrepancies, coupled with their deep-thinking nature, makes deception a futile endeavor. Highly independent and unconventional, they prioritize creative expression over traditional goals, demonstrating remarkable problem-solving skills and an inclination for unbiased solutions. INTPs prefer autonomy, gravitating toward flexible work styles and valuing close-knit circles for unleashing their creativity.",
-    fact: "INTPs can also be referenced as `The Thinker` because of their logical analysis and creative genius.",
+      "Commanders are bold, decisive, and charismatic leaders known for their visionary approach. They thrive on challenges and have a natural ability to rally people behind shared goals. These strategic thinkers excel at problem-solving and enjoy conceptualizing long-term plans to drive change. Competitive and quick-witted, they prefer focusing on the future and abstract ideas over present details. While successful in corporate settings due to their leadership skills, their direct and sometimes blunt communication style can be perceived as harsh. Their emotional distance might lead to clashes in professional settings, emphasizing efficiency over emotional expressions. To maximize their potential, Commanders need to recognize and value the contributions of their teams while fostering emotionally healthy relationships.",
+    fact: "ENTJS make decisions quickly, are very career-focused, and fit well into the corporate world.",
     fact2:
-      "INTPs thrive in work environments that aren't focused on traditions. They prefer flexible and independent work styles. In relationships, they are highly unconventional and autonomous.",
+      "The ENTJ thrives in jobs that are complex and require clear strategies for goals. In relationships, they can set high expectations for their loved ones and can be sometimes domineering.",
     strengths: [
       {
-        title: "Analytical",
-        desc: "Logicians have a knack for spotting patterns and connections through rigorous analysis.",
+        title: "Efficient",
+        desc: "Commanders swiftly identify and rectify inefficiencies, streamlining processes toward their ambitious goals.",
       },
       {
-        title: "Originality",
-        desc: "They generate creative and unconventional ideas driven by their boundless imagination.",
+        title: "Energetic",
+        desc: "Leading invigorates Commanders, boosting their enthusiasm rather than draining it.",
       },
       {
-        title: "Open-Minded",
-        desc: "Logicians embrace new ideas rooted in sound reasoning, fueled by their intense curiosity.",
+        title: "Self-Confident",
+        desc: "They possess unwavering belief in their leadership abilities, asserting opinions confidently.",
       },
       {
-        title: "Objective",
-        desc: "Truth-seekers who value honesty, they combat bias and misinformation with a rational mindset.",
+        title: "Strong-Willed",
+        desc: "Resilient and determined, they persistently overcome challenges.",
+      },
+      {
+        title: "Strategic Thinkers",
+        desc: "They analyze situations comprehensively to move projects forward effectively.",
+      },
+      {
+        title: "Charismatic and Inspiring",
+        desc: "Commanders inspire and rally support behind their ambitious objectives.",
       },
     ],
     weaknesses: [
       {
-        title: "Feeling Overwhelmed",
-        desc: "Social interactions can leave Logicians feeling overwhelmed.",
+        title: "Impatient",
+        desc: "They struggle with delays and slower decision-making.",
       },
       {
-        title: "Struggles with Expression",
-        desc: "Difficulty expressing emotions and forming social connections.",
+        title: "Stubborn and Dominant",
+        desc: "Commanders might insist on their vision without considering others' perspectives.",
       },
       {
-        title: "Over-Analytical",
-        desc: "Tendencies to over-analyze and procrastinate can hinder productivity.",
+        title: "Intolerant",
+        desc: "They prioritize goals over emotionally-rooted ideas, sometimes dismissing them.",
       },
       {
-        title: "Routine Difficulties",
-        desc: "Difficulty adhering to routines and following rules.",
+        title: "Arrogant",
+        desc: "Valuing quick thoughts, they may overlook those who don't match their pace.",
       },
       {
-        title: "Dissatisfaction",
-        desc: " Their constant search for improvement can lead to perpetual dissatisfaction.",
+        title: "Poor Handling of Emotions",
+        desc: "Commanders can be inadvertently insensitive or dismissive of others' feelings.",
+      },
+      {
+        title: "Cold and Ruthless",
+        desc: "Driven by efficiency, they may overlook personal circumstances in their pursuit of goals.",
       },
     ],
     career: {
-      pic: "/images/INTPcareer.svg",
-      summary: "",
+      pic: "/images/ENTJcareer.svg",
+      summary:
+        "Commanders, with their boldness and determination, excel in leadership roles, thriving in careers that allow them to exert authority and push ideas to fruition. Their self-confidence and clear communication make them natural leaders, often gravitating towards executive positions, entrepreneurship, and corporate strategy roles. Structure and efficiency are paramount to Commanders, making them respected in law, academia, and business administration. They enjoy complex challenges and excel in roles that offer variety, leadership, and room for strategic planning. ENTJs are known for their leadership, communication skills, hardworking attitude, and ability to plan for the future, thriving in careers that offer structure and ample opportunities for interaction.",
       jobs: [
-        { job: "Physicist" },
-        { job: "Computer programmer" },
-        { job: "Engineer" },
-        { job: "Software developer" },
-        { job: "Pharmacist" },
-        { job: "Architecture" },
-        { job: "Medicine" },
-        { job: "Musician" },
-        { job: "Artist" },
+        { job: "Human resources manager" },
+        { job: "Company CEO or manager" },
+        { job: "Lawyer" },
+        { job: "Entrepreneur" },
+        { job: "University professor" },
+        { job: "Accountant" },
       ],
     },
     rarity: {
-      text: "INTP is one of the less common types in the population.",
-      desc: "They make up about 4.8% of the general population.",
+      text: "ENTJ is one of the least common types in the population, and the rarest type among women.",
+      desc: "They make up about 1.8% of the general population.",
     },
     celebrities: [
-      { name: "Bill Gates" },
-      { name: "Albert Einstein" },
-      { name: "Tiger Woods" },
-      { name: "Avicii" },
-      { name: "Sheldon Cooper (The Big Bang Theory)" },
-      { name: "Aeomon Targaryan (Game of Thrones)" },
-      { name: "Abraham Lincoln" },
-      { name: "Socrates" },
-      { name: "Charles Darwin" },
+      { name: "Malcolm X" },
+      { name: "Napoleon Bonaparte" },
+      { name: "Margaret Thatcher" },
+      { name: "Kamala Harris" },
+      { name: "Steve Jobs" },
+      { name: "Franklin D. Roosevelt" },
+      { name: "Raymond Reddington (The Blacklist)" },
+      { name: "Doctor Strange (Doctor Strange)" },
+    ],
+  },
+  {
+    personality: "ENTP",
+    title: "Debater",
+    group: "Analysts",
+    pic: "/images/entp-debater-s2-male.png",
+    male: "/images/entp-debater-s2-male.png",
+    female: "/images/entp-debater-s2-female.png",
+    short_desc:
+      "Clever and curious thinkers with a wide range of capabilities and interests.",
+    long_desc:
+      "ENTPs are intellectually curious and inventive thinkers known for their innovation and analytical skills. Referred to as Visionaries or Debaters, they thrive on intellectual challenges, often focusing on ideas rather than details. Quick-witted and logical, they enjoy analyzing theories and facts, but may struggle with follow-through on their numerous ideas. While they relish engaging discussions and verbal sparring, they can be playful and entertaining, yet may not thrive in certain social situations. ENTPs are imaginative, curious, and objective, constantly seeking mental stimulation and embracing their contrarian approach to ideas and debates.",
+    fact: "ENTPss are sometimes referred to as Visionaries or Thinkers because of their ability to grasp difficult concepts and theories.",
+    fact2:
+      "ENTPs are sometimes not the most likable people. While they can strive to be friendly, they don’t seek other people’s approval.",
+    strengths: [
+      {
+        title: "Innovative Thinkers",
+        desc: "Debaters possess a knack for innovation, frequently devising new and original ideas.",
+      },
+      {
+        title: "Communicators",
+        desc: "They excel in debates and discussions, drawing others in with their compelling dialogue.",
+      },
+      {
+        title: "Values Knowledge",
+        desc: "They relish learning and absorbing information, especially regarding abstract concepts.",
+      },
+      {
+        title: "Quick and Flexible Thinking",
+        desc: "Debaters effortlessly shift between ideas, using their accumulated knowledge to make compelling arguments.",
+      },
+      {
+        title: "Charismatic",
+        desc: "Their confident and articulate communication style, coupled with quick wit, captivates audiences.",
+      },
+    ],
+    weaknesses: [
+      {
+        title: "Argumentative",
+        desc: "Their love for debating can create tension by challenging others' beliefs and methods with vigor.",
+      },
+      {
+        title: "Dislike for Routines",
+        desc: "Debaters resist rigid schedules and being controlled, preferring autonomy and flexibility.",
+      },
+      {
+        title: "Intolerant",
+        desc: "They dismiss ideas that lack rational backing, sometimes undermining the individuals behind those ideas.",
+      },
+      {
+        title: "Difficulty Focusing",
+        desc: "Constantly generating new thoughts, they may struggle to concentrate on a single idea for an extended period.",
+      },
+      {
+        title: "Poor Handling of Emotions",
+        desc: "Debaters can be inadvertently insensitive or dismissive of others' feelings.",
+      },
+    ],
+    career: {
+      pic: "/images/ENTPcareer.svg",
+      summary:
+        "ENTPs are non-conformists who thrive in dynamic, intellectually stimulating environments. They excel in careers offering excitement, creative freedom, and challenges. The field of law, where their communication skills and debate prowess shine, or entrepreneurial roles in business, align well with their rationality and leadership capabilities. Debaters prefer jobs involving problem-solving and technical challenges, thriving in careers like entrepreneurship, engineering, and creative fields such as acting or photography. Their adaptability and leadership, coupled with exceptional communication skills, allow them to influence and inspire others, making them well-suited for roles that value intellectual competency, curiosity, and spontaneous engagement.",
+      jobs: [
+        { job: "Engineer" },
+        { job: "Lawyer" },
+        { job: "Scientist" },
+        { job: "Psychologist" },
+        { job: "Psychiatrist" },
+        { job: "Journalist" },
+      ],
+    },
+    rarity: {
+      text: "ENTP is one of the rarer types in the population.",
+      desc: "They make up about 4.3% of the general population.",
+    },
+    celebrities: [
+      { name: "Leonardo da Vinci" },
+      { name: "Niccolo Machiavelli" },
+      { name: "Barack Obama" },
+      { name: "Walt Disney" },
+      { name: "Thomas Edison" },
+      { name: "Celine Dion" },
+      { name: "Captain Jack Sparrow (Pirates of the Caribbean)" },
+      { name: "Tyrion Lannister (Game of Thrones)" },
+      { name: "Jim Halpert (The Office)" },
     ],
   },
 ];
