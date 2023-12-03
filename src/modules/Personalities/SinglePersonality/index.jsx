1bc8f0658eebe86f432569f0a41a8f27d6a874fd
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2023-12-03 14:39:26 +0100

    changes

--- src/modules/Personalities/SinglePersonality/index.jsx
+++ src/modules/Personalities/SinglePersonality/index.jsx
@@ -1,9 +1,34 @@
 import { useParams } from "react-router-dom";
 import "./index.scss";
+import { personalityData } from "./../../../utils/data";
+import Layout from "./../../../components/layout/index";
 
 const SinglePersonality = () => {
   const { personality } = useParams();
-  return <div>SinglePersonality</div>;
+  const data = personalityData?.find((p) => p?.personality === personality);
+  return (
+    <Layout>
+      <div className="singlePersonality">
+        <div className="singlePersonalityHero">
+          <div className="heroText">
+            <h1>{personality}</h1>
+            <div className="line"></div>
+            <h3 className="title">The {data?.title}</h3>
+          </div>
+          <div className="pic">
+            <img src={data?.pic} alt={data?.title} />
+          </div>
+        </div>
+        <div className="summary">
+          <div className="header">
+            <h2>{personality} in a nutshell</h2>
+            <div className="line"></div>
+          </div>
+          <p>{data?.long_desc}</p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default SinglePersonality;
