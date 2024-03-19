c857d366c780d9cc4c3d47e459aba6a7ad919946
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2024-03-19 15:45:39 +0100

    changes

--- src/utils/getPersonality.js
+++ src/utils/getPersonality.js
@@ -1,8 +1,8 @@
 export const getPersonality = (ei, si, tf, jp) => {
-  const eiLetter = ei > 500 ? "E" : "I";
-  const siLetter = si > 500 ? "S" : "N";
-  const tfLetter = tf > 500 ? "T" : "F";
-  const jpLetter = jp > 500 ? "J" : "P";
+  const eiLetter = ei > 350 ? "E" : "I";
+  const siLetter = si > 350 ? "S" : "N";
+  const tfLetter = tf > 350 ? "T" : "F";
+  const jpLetter = jp > 350 ? "J" : "P";
 
   return `${eiLetter}${siLetter}${tfLetter}${jpLetter}`;
 };
